255ba84ba4b7fe62e2770e9b191b3778e1f7ce08
jobinbasani <jobinbasani@gmail.com>
2014-04-03 23:34:23 -0300

    Parse news and links

--- src/com/jobinbasani/news/ml/service/NewsService.java
+++ src/com/jobinbasani/news/ml/service/NewsService.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -12,6 +13,7 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import com.jobinbasani.news.ml.MainActivity;
 import com.jobinbasani.news.ml.constants.NewsConstants;
 import com.jobinbasani.news.ml.receiver.NewsReceiver;
+import com.jobinbasani.news.ml.vo.NewsItem;
 
 import android.app.IntentService;
 import android.content.Intent;
@@ -36,27 +38,99 @@ public class NewsService extends IntentService {
 			     xpp.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 			     xpp.setInput(in, "UTF-8");
 			     int eventType = xpp.getEventType();
+			     ArrayList<NewsItem> newsList = new ArrayList<NewsItem>();
 			     while(eventType != XmlPullParser.END_DOCUMENT){
 			    	 if(xpp.getName()!=null && xpp.getName().equals("description") && eventType==XmlPullParser.START_TAG){
 			    		 xpp.next();
 			    		 String details = xpp.getText().replaceAll("&nbsp;", " ").replaceAll("&raquo;", ">>");
 			    		 if(details.startsWith("<table")){
+			    			 NewsItem mainNews = new NewsItem();
+			    			 NewsItem childNews = new NewsItem();
+			    			 ArrayList<NewsItem> childNewsList = new ArrayList<NewsItem>();
 				    		 XmlPullParser detailsParser = factory.newPullParser();
 				    		 detailsParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 				    		 detailsParser.setInput(new StringReader(details));
 				    		 int detailsEventType = detailsParser.getEventType();
-				    		 String mainImage = "";
+				    		 boolean upcomingMainNewsHeader = false;
+				    		 boolean upcomingMainNewsDetails = false;
+				    		 boolean upcomingMainNewsProvider = false;
+				    		 boolean childNewsStart = true;
+				    		 int fontCounter = 0;
+				    		 int linkCounter = 0;
+				    		 boolean mainHeaderSection = true;
 				    		 while(detailsEventType != XmlPullParser.END_DOCUMENT){
-				    			 if(mainImage.length()==0 && detailsEventType == XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("img")){
-				    				 mainImage = detailsParser.getAttributeValue(null, "src");
-				    				 if(mainImage.startsWith("//"))
-				    					 mainImage = "http:"+mainImage;
-				    				 detailsParser.next();
-				    				 System.out.println(mainImage);
-				    				 continue;
-				    			 }
+				    			 if(mainHeaderSection){
+				    				 if(mainNews.getNewsImageUrl()==null && detailsEventType == XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("img")){
+					    				 mainNews.setNewsImageUrl(detailsParser.getAttributeValue(null, "src"));
+					    				 if(mainNews.getNewsImageUrl().startsWith("//")){
+					    					 mainNews.setNewsImageUrl("http:"+mainNews.getNewsImageUrl());
+					    				 }
+					    				 detailsParser.next();
+					    				 continue;
+					    			 }
+					    			 if(mainNews.getNewsLink()==null && detailsEventType==XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("a")){
+					    				 linkCounter++;
+					    				 if(linkCounter == 2){
+					    					 mainNews.setNewsLink(detailsParser.getAttributeValue(null, "href"));
+						    				 upcomingMainNewsHeader = true;
+					    				 }
+					    				 detailsParser.next();
+					    				 continue;
+					    			 }
+					    			 if(upcomingMainNewsHeader && mainNews.getNewsHeader()==null && detailsEventType==XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("b")){
+					    				 upcomingMainNewsHeader = false;
+					    				 upcomingMainNewsDetails = true;
+					    				 detailsParser.next();
+					    				 mainNews.setNewsHeader(detailsParser.getText());
+					    				 detailsParser.next();
+					    				 continue;
+					    			 }
+				    				 if(upcomingMainNewsDetails && detailsEventType==XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("font")){
+				    					 fontCounter++;
+				    					 if(fontCounter == 2){
+				    						 detailsParser.next();
+				    						 mainNews.setNewsProvider(detailsParser.getText());
+				    						 upcomingMainNewsDetails = false;
+				    						 upcomingMainNewsProvider = true;
+					    				 }
+				    					 detailsParser.next();
+					    				 continue;
+				    				 }
+				    				 
+				    				 if(upcomingMainNewsProvider && detailsEventType==XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("font")){
+			    						 detailsParser.next();
+			    						 detailsParser.next();
+			    						 detailsParser.next();
+			    						 detailsParser.next();
+			    						 detailsParser.next();
+			    						 detailsParser.next();
+			    						 mainNews.setNewsDetails(detailsParser.getText());
+			    						 upcomingMainNewsProvider = false;
+			    						 mainHeaderSection = false;
+				    					 detailsParser.next();
+					    				 continue;
+				    				 }
+				    			 }else{
+				    				 if(childNewsStart && detailsEventType==XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("a")){
+				    					 childNewsStart = false;
+				    					 childNews = new NewsItem();
+				    					 childNews.setNewsLink(detailsParser.getAttributeValue(null, "href"));
+				    					 detailsParser.next();
+				    					 childNews.setNewsHeader(detailsParser.getText());
+				    				 }else if(!childNewsStart && detailsEventType==XmlPullParser.START_TAG && detailsParser.getName()!=null && detailsParser.getName().equals("nobr")){
+				    					 childNewsStart = true;
+				    					 detailsParser.next();
+				    					 childNews.setNewsProvider(detailsParser.getText());
+				    					 if(childNews.getNewsHeader()!=null && childNews.getNewsLink()!=null && childNews.getNewsProvider()!=null){
+				    						 childNewsList.add(childNews);
+				    					 }
+				    				 }
+				    			 } 
+				    			  
 				    			 detailsEventType = detailsParser.next();
 				    		 }
+				    		 mainNews.setChildNewsItems(childNewsList,true);
+				    		 newsList.add(mainNews);
 			    		 }
 			    	 }
 			    	 
--- src/com/jobinbasani/news/ml/vo/NewsItem.java
+++ src/com/jobinbasani/news/ml/vo/NewsItem.java
@@ -10,6 +10,8 @@ public class NewsItem {
 	private String newsImageUrl;
 	private String newsSavedImageName;
 	private String parentId;
+	private String newsLink;
+	private String newsProvider;
 	private ArrayList<NewsItem> childNewsItems;
 	
 	public String getNewsId() {
@@ -22,7 +24,10 @@ public class NewsItem {
 		return newsHeader;
 	}
 	public void setNewsHeader(String newsHeader) {
-		this.newsHeader = newsHeader;
+		if(newsHeader!=null)
+			this.newsHeader = newsHeader;
+		else
+			this.newsHeader = " ";
 	}
 	public String getNewsDetails() {
 		return newsDetails;
@@ -51,8 +56,36 @@ public class NewsItem {
 	public ArrayList<NewsItem> getChildNewsItems() {
 		return childNewsItems;
 	}
+	public String getNewsLink() {
+		return newsLink;
+	}
+	public void setNewsLink(String newsLink) {
+		this.newsLink = newsLink;
+	}
+	public String getNewsProvider() {
+		return newsProvider;
+	}
+	public void setNewsProvider(String newsProvider) {
+		this.newsProvider = newsProvider;
+	}
 	public void setChildNewsItems(ArrayList<NewsItem> childNewsItems) {
 		this.childNewsItems = childNewsItems;
+	}
+	public void setChildNewsItems(ArrayList<NewsItem> childNewsItems, boolean addMainNewsItem) {
+		if(addMainNewsItem){
+			NewsItem childItem = new NewsItem();
+			childItem.setNewsHeader(this.newsHeader);
+			childItem.setNewsLink(this.newsLink);
+			childItem.setNewsProvider(this.newsProvider);
+			childNewsItems.add(childItem);
+			setChildNewsItems(childNewsItems);
+		}else{
+			setChildNewsItems(childNewsItems);
+		}
+	}
+	@Override
+	public String toString() {
+		return "newsId="+newsId+", newsHeader="+newsHeader+", newsDetails="+newsDetails+", newsLink="+newsLink+", newsProvider="+newsProvider;
 	}
 
 }
